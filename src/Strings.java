bc580e85670df1fbdac005db3da8892acdc3a265
Sofia Alves Toreti <sofia.a2005@aluno.ifsc.edu.br>
2024-08-01 15:17:02 -0300

    STRINGS: one question left + change the 0 question to matrices

--- src/Strings.java
+++ src/Strings.java
@@ -1,5 +1,6 @@
 package src;
 import java.util.*;
+import java.util.Arrays;
 
 public class Strings {
 	public static void main(String[] args) {
@@ -8,39 +9,74 @@ public class Strings {
 1. Escreva um programa que remova todas as vogais de uma string fornecida pelo
 usuário
 
-2. Escreva um programa que leia uma string fornecida pelo usuário e a imprima
-invertida.
-
 3. Escreva um programa que leia uma string fornecida pelo usuário e imprima
 quantas vezes cada letra aparece na string.
  */
 
-		char[] vogais = {'a','e','i','o','u'};
+		Scanner scanner = new Scanner(System.in);
+		String[] vogais = {"a","e","i","o","u"};
 		int cont = 0;
 
-		Scanner scanner = new Scanner(System.in);
 		System.out.print("digite um texto qualquer: ");
-		String str = scanner.nextLine();
-		str = str.toLowerCase();
+		String input = scanner.nextLine().toLowerCase();
+
+		
+// q-2: apague todas as vogais da string fornecida.
+		String consoante = input;
+		for (int i = 0; i < vogais.length; i++) {
+			consoante = consoante.replace(vogais[i], "");
+		}
+		System.out.println("\n-2. apenas consoantes: " + consoante + ".\n");
+
+
+// q-1: imprima a string fornecida invertida.
+		String invertida = "";
+		for (int i = input.length() - 1; i >= 0; i--) {
+			invertida = invertida + String.valueOf(input.charAt(i));
+		}
+		System.out.println("-1. " + input + " invertido fica: " + invertida + ".\n");
+
+
+// q0: imprima quantas vezes cada letra de uma palavra é repetida.
+		String repetição = input.replace(" ","");
+		System.out.println("0. número de repetição de letras: ");
+		for (int i = 0; i < repetição.length(); i++) {
+
+			cont = 0;
+			char letter = repetição.charAt(0);
+			System.out.print(letter + ": ");
+
+			for (int j = 0; j < repetição.length(); j++) {
+				if (letter == repetição.charAt(j)) {
+					cont ++;
+				}
+			}
+
+			repetição = repetição.replace(String.valueOf(letter),"");
+
+			System.out.println(cont + ".");
+
+		}
 
 
 // q1: programa que pessa uma string para um usuário e conte o número de carateries.
-		System.out.println("1. número de carateries da string inserida: " + str.length() + ".\n");
+		System.out.println("\n1. número de carateries da string inserida: " + input.length() + ".\n");
 
 
 // q2: verifique se a palavra é um palídromo.
-		String palindromo = new StringBuilder(str).reverse().toString().replace(" ", "");
-		if (str.replace(" ","").equals(palindromo)) {
-			System.out.println("2. " + str + " é um palíndromo.\n");
+		String palindromo = invertida.replace(" ", "");
+		if (input.replace(" ","").equals(palindromo)) {
+			System.out.println("2. " + input + " é um palíndromo.\n");
 		} else {
-			System.out.println("2. " + str + " não é um palíndromo.\n");
+			System.out.println("2. " + input + " não é um palíndromo.\n");
 		}
-		System.out.println(palindromo);
+
 
 // q3: conte o número de vogais.
-		for (int i = 0; i < str.length(); i++) {
+		cont = 0;
+		for (int i = 0; i < input.length(); i++) {
 			for (int j = 0; j < vogais.length; j++) {
-				if (str.charAt(i) == vogais[j]) {
+				if (String.valueOf(input.charAt(i)).equals(vogais[j])) {
 					cont++;
 				}
 			}
@@ -49,45 +85,92 @@ quantas vezes cada letra aparece na string.
 
 
 // q4: remova os espaços de uma string.
-		System.out.println("4. sem espaços: " + str.replace(" ","") + ".\n");
-
+		System.out.println("4. sem espaços: " + input.replace(" ","") + ".\n");
 
 
 // q5: remova os espaços de uma string.
 		System.out.print("5. digite um símbolo para substituir os espaços: ");
 		String symbol = scanner.nextLine();
-		System.out.println("sem espaços: " + str.replace(" ",symbol.substring(0,1)) + ".\n");
+		System.out.println("sem espaços: " + input.replace(" ",symbol.substring(0,1)) + ".\n");
 
 
 // q6: conte o número de palavras.
-		cont = 0;
-		if (!str.isBlank()) {
-			cont++;
-			for (int i = 0; i < str.length(); i++) {
-				String letter = String.valueOf(str.charAt(i));
+		int palavras = 0;
+		if (!input.isBlank()) {
+			palavras++;
+			for (int i = 0; i < input.length(); i++) {
+				String letter = String.valueOf(input.charAt(i));
 				if (letter.equals(" ")) {
-					cont++;
+					palavras++;
 				}
 			}
 		}
-		System.out.println("6. você digitou " + cont + " palavras.\n");
+		if (palavras == 1) {
+			System.out.println("6. você digitou " + palavras + " palavra.\n");
+		} else {
+			System.out.println("6. você digitou " + palavras + " palavras.\n");
+		}
 
 
 // q7: descubra a maior palavra escrita.
-//        System.out.println("7. você digitou " + cont + " palavras.\n");
+		String text = input;
+		int index = 0;
+		String[] words = new String[palavras];
+
+		// separating all the words.
+		for (int i = 0; i < palavras; i++) {
+			if (i == palavras - 1) {
+				// only one word.
+				words[i] = text;
+			} else {
+				// more than one words left.
+				index = text.indexOf(" ");
+				words[i] = text.substring(0,index);
+				text = text.substring(index + 1);
+			}
+		}
+
+		if (palavras == 0) {
+			// if the input string is null.
+			System.out.println("7. nenhuma palavra foi fornecida.\n");
+
+		} else {
+			// if the input string is NOT null.
+
+			String maior = words[0];
+			cont = words[0].length();
+
+			if (palavras > 1)
+				// if only there is more than 1 word.
+				for (int i = 1; i < words.length; i++) {
+					if (cont < words[i].length()) {
+						maior = words[i];
+						cont = words[i].length();
+					}
+				}
+
+			System.out.println("7. a maior palavra escrita é: " + maior + ".\n");
+
+		}
 
 
-// q8: descubra a maior palavra escrita.
-//        System.out.println("8. você digitou " + cont + " palavras.\n");
+// q8: pessa outro input do usuário e descubra se é um anagrama da primeira string.
+		System.out.print("digite um outro texto qualquer: ");
+		String anagrama = scanner.nextLine().toLowerCase();
+
+//      System.out.println("8. as palavras " + input + " e " + + " são anagramas.\n");
 
 
 // q9: transforme a string fornecida para camalCase
-//        System.out.println("9. você digitou " + cont + " palavras.\n");
+		String camalCase = words[0];
+		for (int i = 1; i < words.length; i++) {
+			camalCase = camalCase + words[i].substring(0,1).toUpperCase() + words[i].substring(1);
+		}
+
+      System.out.println("9. camalCase: " + camalCase + ".\n");
 
 
-// q0: transforme a string fornecida para camalCase
-//        System.out.println("0. você digitou " + cont + " palavras.\n");
-
+// fim.
 		scanner.close();
 	}
 }
